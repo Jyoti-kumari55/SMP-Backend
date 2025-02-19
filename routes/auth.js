b8eb42d65f87def832c1c21e718ab294b520fa25
Jyoti Kumari <68436635+Jyoti-kumari55@users.noreply.github.com>
2025-02-19 13:24:48 +0530

    logout-updated

--- routes/auth.js
+++ routes/auth.js
@@ -91,7 +91,7 @@ router.get("/logout", (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      
     });
     res.status(200).json({ message: `You are successfully logged out.` });
   } catch (error) {
