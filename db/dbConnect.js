5218130abad3db5eba55377e551427dcf4af5380
Jyoti Kumari <68436635+Jyoti-kumari55@users.noreply.github.com>
2025-02-19 12:37:06 +0530

    timeouts added

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -5,7 +5,6 @@ const mongoURI = process.env.MONGODB;
 const connectionOptions = {
   useNewUrlParser: true,  
   useUnifiedTopology: true,  
-  serverSelectionTimeoutMS: 5000,  
   socketTimeoutMS: 45000, 
   connectTimeoutMS: 10000,  
 };
